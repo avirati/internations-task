f23efcf126345119e336b0ef97eefc90d261cbc7
Avinash Verma <eyedagger@gmail.com>
2016-05-16 01:07:52 +0530

    User form now has ability to tag groups

--- app/js/user.js
+++ app/js/user.js
@@ -3,28 +3,48 @@
 
   $U('#add-user-btn').addEventListener('click', function () {
       var _userName = $U('#user-name').value;
-
-      if(_userName) {
-        var _users = $storage.get('users') || [];
-
-        for(var i = 0, iL = _users.length, user = _users[i]; i < iL; i++) {
-          if(user.name === _userName) {
-            _window.toast('User already exists');
-            return;
-          }
+      var _groups = (function() {
+        var _groupCheckboxes = _document.getElementsByName("groupName[]") || [];
+        var _arr = [];
+        for(var i = 0, iL = _groupCheckboxes.length; i < iL; i++) {
+          if(_groupCheckboxes[i].checked)
+            _arr.push(_groupCheckboxes[i].value);
         }
+        return _arr;
+      })()
 
-        _users.push({
-          name: _userName,
-          users: []
-        })
-
-        $storage.set('users', _users);
-
-        $U('#add-new-user').classList.remove('open');
-
-        renderUsers(_users);
+      if(!_userName) {
+        _window.toast("Enter a valid user name");
+        return;
       }
+
+      if(_groups.length === 0) {
+        _window.toast("A user must be associated with atleast 1 group");
+        return;
+      }
+
+      var _users = $storage.get('users') || [];
+
+      for(var i = 0, iL = _users.length; i < iL; i++) {
+        var _user = _users[i];
+        if(_user.name === _userName) {
+          _window.toast('User already exists');
+          return;
+        }
+      }
+
+      console.log(_groups)
+
+      _users.push({
+        name: _userName,
+        groups: _groups
+      })
+
+      $storage.set('users', _users);
+
+      $U('#add-new-user').classList.remove('open');
+
+      renderUsers(_users);
   });
 
   function renderUsers (users) {
@@ -38,16 +58,24 @@
 
       var _deleteButton = _document.createElement('button');
       _deleteButton.className = "btn btn-circle delete-user right";
-      if(user.users.length !== 0) {
-        _deleteButton.classList.add('disabled');
-      }
       _deleteButton.innerHTML = '-';
       _deleteButton.setAttribute('data-delete-user', user.name);
       _deleteButton.addEventListener('click', function (event) {
         var _userName = event.target.getAttribute('data-delete-user');
-        if(user.users.length === 0) {
-          deleteUser(_userName);
+
+        var _groups = $storage.get('groups') || [];
+
+        for(var i = 0, iL = _groups.length; i < iL; i++) {
+          var _group = _groups[i];
+          var index;
+
+          if((index = _group.users.indexOf(_userName)) > -1) {
+            _group.users.splice(index, 1);
+          }
         }
+
+        $storage.set('groups', _groups);
+        renderUsers();
       })
 
       _li.appendChild(_deleteButton);
@@ -67,6 +95,25 @@
     }
   }
 
-  renderUsers($storage.get('users') || [])
+  function populateGroups (groups) {
+    for(var i = 0, iL = groups.length; i < iL; i++) {
+      var _groupCheckbox = _document.createElement('input');
+      _groupCheckbox.type = "checkbox";
+      _groupCheckbox.name = "groupName[]";
+      _groupCheckbox.value = groups[i].name;
+
+      var _groupLabel = _document.createElement('label');
+      _groupLabel.innerHTML = groups[i].name;
+
+      var _groupContainer = _document.createElement('div');
+      _groupContainer.appendChild(_groupCheckbox);
+      _groupContainer.appendChild(_groupLabel);
+
+      $U('#group-list').appendChild(_groupContainer);
+    }
+  }
+
+  renderUsers($storage.get('users') || []);
+  populateGroups($storage.get('groups') || []);
 
 })(window, document, $, $U, Storage)
